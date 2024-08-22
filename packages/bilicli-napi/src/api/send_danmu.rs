277dc1e7afe24a64acd54f18f5e4f90ec632a883
starknt <1431880400@qq.com>
2024-08-23 07:06:44 +0800

    genrate rng

--- packages/bilicli-napi/src/api/send_danmu.rs
+++ packages/bilicli-napi/src/api/send_danmu.rs
@@ -1,12 +1,39 @@
+#![allow(unused)]
+
 use std::collections::HashMap;
 
+use chrono::{Local, Timelike};
 use reqwest::header::{HeaderMap, HeaderValue, COOKIE};
 use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct BaseUserInfo {
+    name: String,
+    face: String,
+}
+
+#[derive(Deserialize)]
+struct User {
+    uid: u64,
+    base: BaseUserInfo,
+}
+
+#[derive(Deserialize)]
+struct ModeInfo {
+    mode: i32,
+    user: User,
+}
+
+#[derive(Deserialize)]
+struct SendDanmuData {
+    mode_info: ModeInfo,
+}
 
 #[derive(Deserialize)]
 struct SendDanmuResponse {
     code: i32,
     message: String,
+    data: Option<SendDanmuData>,
 }
 
 pub async fn send_danmu(room_id: u32, content: &str, cookie: String) -> Result<(), String> {
@@ -31,7 +58,7 @@ pub async fn send_danmu(room_id: u32, content: &str, cookie: String) -> Result<(
     params.insert("fontsize", "25".to_string());
     params.insert("mode", "1".to_string());
     params.insert("msg", content.to_string());
-    params.insert("rnd", "1724362289".to_string());
+    params.insert("rnd", Local::now().timestamp().to_string());
     params.insert("roomid", room_id.to_string());
 
     let client = reqwest::Client::new();
