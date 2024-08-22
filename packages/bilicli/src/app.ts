0708358613e38e7c4edeeb5a0c10989899105bfb
starknt <1431880400@qq.com>
2024-08-23 01:24:14 +0800

    feat: support `uid` input

--- packages/bilicli/src/app.ts
+++ packages/bilicli/src/app.ts
@@ -1,9 +1,10 @@
 import { Cli, MsgType } from '@natmri/bilicli-napi'
-import { AttentionChangeMsg, MsgHandler, startListen, WatchedChangeMsg } from 'blive-message-listener'
+import { Message, MsgHandler, startListen } from 'blive-message-listener'
 
 export interface AppOptions {
   cookie?: string
   config?: string
+  uid?: string
 }
 
 export interface EditOptions {
@@ -11,51 +12,45 @@ export interface EditOptions {
 }
 
 export class App {
-  private cli!: Cli
+  private readonly roomId: number
+  private readonly cli: Cli
 
-  constructor(readonly roomId: number, private options: AppOptions) {
+  constructor(roomId: string, private options: AppOptions) {
+    this.roomId = parseInt(roomId)
+
+    if (isNaN(this.roomId)) {
+      throw new Error('房间号格式不正确')
+    }
+
     this.cli = new Cli(this.roomId)
 
     const handler: MsgHandler = {
-      onAttentionChange: ({ body }) => this.handleAttentionChange(body),
-      onWatchedChange: ({ body }) => this.handleWatchedChange(body),
+      onAttentionChange: ({ body }) => this.cli.sendAttentionChange(body.attention),
+      onWatchedChange: ({ body }) => this.cli.sendWatcherChange(body.num),
       onLiveStart: () => this.cli.sendLiveChange(true),
       onLiveEnd: () => this.cli.sendLiveChange(false),
-      onIncomeDanmu: (msg) => {
-        this.cli.sendMsg(MsgType.Danmu, JSON.stringify(msg.body))
-      },
-      onIncomeSuperChat: (msg) => {
-        this.cli.sendMsg(MsgType.SuperChat, JSON.stringify({
-          ...msg.body,
-          timestamp: msg.timestamp,
-        }))
-      },
-      onGift: (msg) => {
-        this.cli.sendMsg(MsgType.Gift, JSON.stringify({
-          ...msg.body,
-          timestamp: msg.timestamp,
-        }))
-      },
-      onGuardBuy: (msg) => {
-        this.cli.sendMsg(MsgType.GuardBuy, JSON.stringify({
-          ...msg.body,
-          timestamp: msg.timestamp,
-        }))
-      },
-      onUserAction: (msg) => {
-        this.cli.sendMsg(MsgType.UserAction, JSON.stringify({
-          ...msg.body,
-          timestamp: msg.timestamp,
-        }))
-      },
+      onIncomeDanmu: (msg) => this.cli.sendMsg(MsgType.Danmu, JSON.stringify(this.mixTimestamp2Body(msg))),
+      onIncomeSuperChat: (msg) =>  this.cli.sendMsg(MsgType.SuperChat, JSON.stringify(this.mixTimestamp2Body(msg))),
+      onGift: (msg) => this.cli.sendMsg(MsgType.Gift, JSON.stringify(this.mixTimestamp2Body(msg))),
+      onGuardBuy: (msg) => this.cli.sendMsg(MsgType.GuardBuy, JSON.stringify(this.mixTimestamp2Body(msg))),
+      onUserAction: (msg) => this.cli.sendMsg(MsgType.UserAction, JSON.stringify(this.mixTimestamp2Body(msg))),
     }
-    
+
+    let uid: number | undefined
+    if (this.options?.uid) {
+      uid = parseInt(this.options.uid)
+      if (isNaN(uid)) {
+        throw new Error('UID 格式不正确')
+      }
+    }
+
     startListen(this.roomId, handler, {
       ws: {
         headers: {
-          'Cookie': this.options?.cookie || '',
-        }
-      }
+          'Cookie': this.options?.cookie || "",
+        },
+        uid,
+      },
     })
   }
 
@@ -63,11 +58,10 @@ export class App {
     await this.cli.run()
   }
 
-  private handleAttentionChange(body: AttentionChangeMsg) {
-    this.cli.sendAttentionChange(body.attention)
-  }
-
-  private handleWatchedChange(body: WatchedChangeMsg) {
-    this.cli.sendWatcherChange(body.num)
+  private mixTimestamp2Body(msg: Message<any>) {
+    return {
+      ...msg.body,
+      timestamp: msg?.timestamp || Date.now(),
+    }
   }
 }
--- packages/bilicli/src/index.ts
+++ packages/bilicli/src/index.ts
@@ -7,11 +7,12 @@ import { App, AppOptions, EditOptions } from './app'
 const cli = cac('bilicli')
 
 cli
-  .command('<room_id>', 'Input will be listen room id, and open live console')
-  .option('-c, --cookie <cookie>', 'Cookie for login')
-  .option('--config <config>', 'Config file path')
+  .command('<room_id>', '输入房间号，打开直播间控制台')
+  .option('-c, --cookie <cookie>', '将你在B站登录的cookie粘贴到这里')
+  .option('-u, --uid <uid>', '你的B站UID')
+  .option('--config <config>', '配置文件路径')
   .action(async (roomId: string, options: AppOptions) => {
-    const app = new App(~~roomId, options)
+    const app = new App(roomId, options)
     await app.run()
     process.exit(0)
   })
